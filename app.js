a4c2b028b5ab0d29e6e504b54d21b1b66df93d86
Nishant <your-email@example.com>
2026-03-14 17:30:02 +0530

    remove body-parser use express built-in parser2

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ import {
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", renderHomePage);
--- controllers/bookcontroller.js
+++ controllers/bookcontroller.js
@@ -1,5 +1,4 @@
 
-import bodyParser from "body-parser";
 import {
   getAllBooks,
   addBook,
